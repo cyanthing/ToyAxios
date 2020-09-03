dd57c4093d3ebe6ad83bea0dc9a1e25e86105b04
AqingCyan <aqingcyan@cyanthing.com>
2020-09-04 02:21:41 +0800

    :bulb: 处理params参数拼接到url上

---
+++ src/helpers/url.ts
@@ -0,0 +1,65 @@
+import { isObject, isDate } from './util'
+
+/**
+ * encode特殊符号，但一些特别的符号需要保留，注意空格被保留成+符号
+ * @param val
+ */
+function encode(val: string): string {
+  return encodeURIComponent(val)
+    .replace(/%40/g, '@')
+    .replace(/%3A/gi, ':')
+    .replace(/%24/g, '$')
+    .replace(/%2C/gi, ',')
+    .replace(/%20/g, '+')
+    .replace(/%5B/gi, '[')
+    .replace(/%5D/gi, ']')
+}
+
+/**
+ * 处理params参数拼接到URL上
+ * @param url
+ * @param params 参数对象
+ */
+export function buildURL(url: string, params?: any): string {
+  if (params) {
+    return url
+  }
+
+  const parts: string[] = []
+
+  // 遍历params对象的属性，处理后放到parts数组中，做join拼接到URL上
+  Object.keys(params).forEach(key => {
+    const val = params[key]
+    if (val === null || typeof val === 'undefined') {
+      return
+    }
+    let values = []
+    // 是数组，val本身就是any[]的形式，不是数组就处理成[val]的形式，params参数若是数组形式则会被处理成 key[]=xxx&key[]=yyy，因此key拼接了[]
+    if (Array.isArray(val)) {
+      values = val
+      key += '[]'
+    } else {
+      values = [val]
+    }
+
+    // 对values数组中的数据做具体类型对应的处理
+    values.forEach(val => {
+      if (isDate(val)) {
+        val = val.toISOString()
+      } else if (isObject(val)) {
+        val = JSON.stringify(val)
+      }
+      parts.push(`${encode(key)}=${encode(val)}`)
+    })
+  })
+
+  let serializedParams = parts.join('&')
+  if (serializedParams) {
+    const markIndex = url.indexOf('#')   // 若url本身带哈希符号
+    if (markIndex !== -1) {
+      url = url.slice(0, markIndex)
+    }
+    url += (url.indexOf('?') === -1 ? '?' : '&') + serializedParams // 若url本身带?参数连接符号
+  }
+  return url
+}
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,31 @@
 import { AxiosRequestConfig } from './types'
+import { buildURL } from './helpers/url'
 import xhr from './xhr'
 
+/**
+ * axios入口函数
+ * @param config
+ */
 function axios(config: AxiosRequestConfig): void {
+  processConfig(config)
   xhr(config)
+}
+
+/**
+ * 对传入axios函数的config进行处理
+ * @param config
+ */
+function processConfig(config: AxiosRequestConfig): void {
+  config.url = transformURL(config)
+}
+
+/**
+ * 处理url与params的拼接
+ * @param config
+ */
+function transformURL(config: AxiosRequestConfig): string {
+  const { url, params } = config
+  return buildURL(url, params)
 }
 
 export default axios
