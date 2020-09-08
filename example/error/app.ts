8df80a5f1bc45dafb711c35dbf0188f77b80f8ce
AqingCyan <aqingcyan@cyanthing.com>
2020-09-09 00:49:12 +0800

    :tada: 错误增强处理demo测试

--- example/error/app.ts
+++ example/error/app.ts
@@ -1,4 +1,4 @@
-import axios from '../../src/index'
+import axios, { AxiosErrorType } from '../../src/index'
 
 axios({
   method: 'get',
@@ -37,4 +37,19 @@ axios({
   console.log(res)
 }).catch((e) => {
   console.log(e)
+})
+
+
+axios({
+  method: 'get',
+  url: '/error/timeout',
+  timeout: 2000
+}).then((res) => {
+  console.log(res)
+}).catch((e: AxiosErrorType) => {
+  console.log(e.message)
+  console.log(e.config)
+  console.log(e.code)
+  console.log(e.request)
+  console.log(e.isAxiosError)
 })
